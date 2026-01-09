668d284620828c3457613151a8280b00d0be44c5
nick23447 <156142540+nick23447@users.noreply.github.com>
2026-01-09 15:06:22 -0800

    clean up imports and move in open with command

--- taskflow/__init__.py
+++ taskflow/__init__.py
@@ -1,28 +1,30 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
 from taskflow.config import Config
-from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 import sqlalchemy as sa
 
 db: SQLAlchemy= SQLAlchemy()
-migrate: Migrate = Migrate()
 bcrypt: Bcrypt = Bcrypt()
+jwt = JWTManager()
 
 def create_app(config_class: type[Config] = Config) -> Flask:
     app: Flask = Flask(__name__)
     app.config.from_object(config_class)
 
+    CORS(app)
+
     db.init_app(app)
     bcrypt.init_app(app)
-    migrate.init_app(app, db)
+    jwt.init_app(app)
     
     from taskflow.users.routes import users
     
     app.register_blueprint(users)
 
-    engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-    inspector = sa.inspect(engine)
+    with app.app_context():
+        db.create_all()
 
     return app

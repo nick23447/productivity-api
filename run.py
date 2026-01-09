78b530b9714dcd055e73a1601cdc19cacb0a17e5
nick23447 <156142540+nick23447@users.noreply.github.com>
2026-01-09 15:05:50 -0800

    move open with command to taskflow

--- run.py
+++ run.py
@@ -3,7 +3,5 @@ from taskflow import create_app, db
 app = create_app()
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True,port=8080)
+    app.run(host="127.0.0.1", port=8080, debug=True)
 

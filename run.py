1be8bbf4140bb67eff619efcbab5c3e58c8c0623
nick23447 <156142540+nick23447@users.noreply.github.com>
2026-01-10 18:06:40 -0800

    change port number

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from taskflow import create_app, db
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="127.0.0.1", port=5001, debug=True)
 
